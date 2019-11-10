8a971dfe996aea84a3095256e1ce50e76a3e51df
Samuel <samuelsimao.dev@gmail.com>
2019-11-10 19:26:37 -0300

    Iniciar CircleCI
    Um teste que provavelmente falhará.

--- central-erros/src/test/java/br/com/central/erros/impl/CentralErrosApplicationTests.java
+++ central-erros/src/test/java/br/com/central/erros/impl/CentralErrosApplicationTests.java
@@ -1,13 +1,13 @@
 package br.com.central.erros.impl;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CentralErrosApplicationTests {
-
 	@Test
 	void contextLoads() {
+		Assert.assertEquals(Boolean.TRUE, Boolean.TRUE);
 	}
-
 }
