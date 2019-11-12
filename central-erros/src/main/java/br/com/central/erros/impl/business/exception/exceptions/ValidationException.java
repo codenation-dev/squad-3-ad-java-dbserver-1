89a09d8661eeb1083a277c42daee18934ab81df2
samueldcs <mucapapipa.br@gmail.com>
2019-11-12 13:20:04 -0300

    Revert "Merge branch 'master' into samuel-circleci"
    This reverts commit 258668b28316ad18443fb6c8a5a23fd2bef055ae.

--- central-erros/src/main/java/br/com/central/erros/impl/business/exception/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package br.com.central.erros.impl.business.exception.exceptions;
-
-public class ValidationException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public ValidationException(String s) {
-        super(s);
-    }
-}
--- central-erros/src/test/java/br/com/central/erros/impl/CentralErrosApplicationTests.java
+++ central-erros/src/test/java/br/com/central/erros/impl/CentralErrosApplicationTests.java
@@ -1,7 +1,8 @@
 package br.com.central.erros.impl;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 class CentralErrosApplicationTests {
 	@Test
