aa9582788b46e80e5a32f87cf2fb9ff8e4862827
Samuel <samuelsimao.dev@gmail.com>
2019-12-07 20:29:10 -0300

    Otimiza imports, padroniza linguagem dos testes e amplia test coverage dos converters

--- central-erros/src/test/java/br/com/central/erros/impl/entity/converter/LogConverterTest.java
+++ central-erros/src/test/java/br/com/central/erros/impl/entity/converter/LogConverterTest.java
@@ -1,14 +1,12 @@
 package br.com.central.erros.impl.entity.converter;
 
 import br.com.central.erros.impl.business.dto.LogDTOV1;
-import br.com.central.erros.impl.business.dto.UserDTOV1;
 import br.com.central.erros.impl.business.entity.V1.LogV1;
 import br.com.central.erros.impl.business.entity.V1.UserV1;
 import br.com.central.erros.impl.business.entity.converter.LogConverter;
-import br.com.central.erros.impl.business.entity.converter.UserConverter;
 import br.com.central.erros.impl.business.entity.enums.Ambiente;
 import br.com.central.erros.impl.business.entity.enums.Level;
-import br.com.central.erros.impl.business.entity.enums.TipoUser;
+import br.com.central.erros.impl.business.exception.exceptions.ValidationException;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -17,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LogConverterTest {
     @Test
-    public void convertsFromDTOToEntity() {
+    public void converteDeDTOParaEntidade() {
 
         LogV1 expected = new LogV1(null, "127.0.0.1", 2L, LocalDate.now(), "", "",
                 Ambiente.DEVELOPMENT, Level.DEBUG, new UserV1());
@@ -30,7 +28,7 @@ public class LogConverterTest {
     }
 
     @Test
-    public void convertsFromEntityToDTO() {
+    public void converteDeEntidadeParaDTO() {
         LogDTOV1 expected = new LogDTOV1("127.0.0.1", 2L, LocalDate.now(), "", "",
                 Ambiente.DEVELOPMENT, Level.DEBUG, new UserV1());
 
@@ -39,5 +37,15 @@ public class LogConverterTest {
                 expected.getUserV1());
 
         assertThat(expected).isEqualToComparingFieldByFieldRecursively(LogConverter.logToDTO(entity));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void retornaExcessaoQuandoTentaConverterLogNulaParaDTO() {
+        LogConverter.logToDTO(null);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void retornaExcessaoQuandoTentaConverterDTONuloParaLog() {
+        LogConverter.logDTOToEntity(null);
     }
 }
--- central-erros/src/test/java/br/com/central/erros/impl/entity/converter/UserConverterTest.java
+++ central-erros/src/test/java/br/com/central/erros/impl/entity/converter/UserConverterTest.java
@@ -4,15 +4,15 @@ import br.com.central.erros.impl.business.dto.UserDTOV1;
 import br.com.central.erros.impl.business.entity.V1.UserV1;
 import br.com.central.erros.impl.business.entity.converter.UserConverter;
 import br.com.central.erros.impl.business.entity.enums.TipoUser;
+import br.com.central.erros.impl.business.exception.exceptions.ValidationException;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserConverterTest {
 
     @Test
-    public void convertsFromDTOToEntity() {
+    public void converteDeDTOParaEntidade() {
         UserV1 expected = new UserV1(null, "Mario", "m@ario.com", "123", TipoUser.PESSOAFISICA,
                 "123");
         UserDTOV1 dto = new UserDTOV1(expected.getNome(), expected.getEmail(), expected.getCpfOuCnpj(),
@@ -21,11 +21,21 @@ public class UserConverterTest {
     }
 
     @Test
-    public void convertsFromEntityToDTO() {
+    public void converteDeEntidadeParaDTO() {
         UserDTOV1 expected = new UserDTOV1("Mario", "m@ario.com", "123", TipoUser.PESSOAFISICA,
                 "123");
         UserV1 entity = new UserV1(null, expected.getNome(), expected.getEmail(), expected.getCpfOuCnpj(),
                 expected.getTipo(), expected.getSenha());
         assertThat(expected).isEqualToComparingFieldByFieldRecursively(UserConverter.userToDTO(entity));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void retornaExcessaoQuandoTentaConverterUsuarioNuloParaDTO() {
+        UserConverter.userToDTO(null);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void retornaExcessaoQuandoTentaConverterDTONuloParaUsuario() {
+        UserConverter.userDTOToEntity(null);
     }
 }
