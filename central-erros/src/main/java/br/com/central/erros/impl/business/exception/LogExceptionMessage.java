720e7bfa118c706caf1b6b1f11ec86586f3bd944
Samuel <samuelsimao.dev@gmail.com>
2019-12-07 20:23:16 -0300

    Adicionar novos erros/exceptions

--- central-erros/src/main/java/br/com/central/erros/impl/business/exception/LogExceptionMessage.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/exception/LogExceptionMessage.java
@@ -1,7 +1,5 @@
 package br.com.central.erros.impl.business.exception;
 
-
 public abstract class LogExceptionMessage {
-    //ERRO + CONPORTAMENTO + ENTIDADE ASSOCIADA
-    public static final String ERROR_DADOS_INVALIDOS = "Dados de log invalido!";
+    public static final String ERRO_DADOS_DA_LOG_INVALIDOS = "Dados de log invalido!";
 }
