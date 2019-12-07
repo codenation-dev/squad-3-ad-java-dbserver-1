a80e4da93d914ddc97694d7cb1d6f8a31ebfdc8e
Samuel <samuelsimao.dev@gmail.com>
2019-12-07 20:22:25 -0300

    Melhora e corrige converters

--- central-erros/src/main/java/br/com/central/erros/impl/business/entity/converter/UserConverter.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/entity/converter/UserConverter.java
@@ -5,32 +5,32 @@ import java.util.stream.Collectors;
 
 import br.com.central.erros.impl.business.dto.UserDTOV1;
 import br.com.central.erros.impl.business.entity.V1.UserV1;
+import br.com.central.erros.impl.business.entity.enums.Perfil;
 import br.com.central.erros.impl.business.exception.UserExceptionMessage;
 import br.com.central.erros.impl.business.exception.exceptions.ValidationException;
 
 
 public class UserConverter {
 
-
-    public static UserV1 userDTOToEntity(UserDTOV1 userDTOV1) {
-
-        if (Objects.isNull(userDTOV1)) {
-            throw new ValidationException(UserExceptionMessage.ERROR_DADOS_INVALIDOS_USER);
+    private static void jogarExcessaoSeUsuarioNulo(Object usuario) {
+        if (Objects.isNull(usuario)) {
+            throw new ValidationException(UserExceptionMessage.ERRO_DADOS_DO_USUARIO_INVALIDOS);
         }
+    }
 
-        return new UserV1(null, userDTOV1.getNome(), userDTOV1.getEmail(), userDTOV1.getCpfOuCnpj(), userDTOV1.getTipo(), userDTOV1.getSenha());
+    public static UserV1 userDTOToEntity(UserDTOV1 userDTO) {
+        jogarExcessaoSeUsuarioNulo(userDTO);
+        return new UserV1(null, userDTO.getNome(), userDTO.getEmail(), userDTO.getCpfOuCnpj(),
+                userDTO.getTipo(), userDTO.getSenha());
     }
 
 
     public static UserDTOV1 userToDTO(UserV1 user) {
-
-        if (Objects.isNull(user)) {
-            throw new ValidationException(UserExceptionMessage.ERROR_DADOS_INVALIDOS_USER);
-        }
-
-        UserDTOV1 userDTOV1 = new UserDTOV1(user.getNome(), user.getEmail(), user.getCpfOuCnpj(), user.getTipo(), user.getSenha());
-        userDTOV1.setPerfis(user.getPerfis().stream().map(value -> value.getCod()).collect(Collectors.toSet()));
-        return userDTOV1;
+        jogarExcessaoSeUsuarioNulo(user);
+        UserDTOV1 dto = new UserDTOV1(user.getNome(), user.getEmail(), user.getCpfOuCnpj(), user.getTipo(),
+                user.getSenha());
+        dto.setPerfis(user.getPerfis().stream().map(Perfil::getCod).collect(Collectors.toSet()));
+        return dto;
     }
 
 }
