ab1c59441ba72e8526670e92afd1ed2d5b8b79e5
romulo krebs <romulo.krebs@gmail.com>
2019-12-08 21:53:19 -0300

    endpoint logs com parametros

--- central-erros/src/main/java/br/com/central/erros/impl/business/repository/V1/LogRepositoryV1.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/repository/V1/LogRepositoryV1.java
@@ -4,7 +4,6 @@ package br.com.central.erros.impl.business.repository.V1;
 import java.util.List;
 
 import br.com.central.erros.impl.business.entity.V1.LogV1;
-import br.com.central.erros.impl.business.entity.enums.Ambiente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface LogRepositoryV1 extends JpaRepository<LogV1, Integer> {
 
     List<LogV1> findByUserV1Id(Integer id);
-   // List<LogV1> findByLevel(Level descricao);
-    List<LogV1> findByAmbiente(Ambiente ambiente);
-   // List<LogV1> findByDetalhesContaining(String detalhes);
+    List<LogV1> findByLevelDescricao(String descricao);
+    List<LogV1> findByAmbienteDescricao(String ambiente);
+    List<LogV1> findByDetalhesContaining(String detalhes);
 
 
 }
