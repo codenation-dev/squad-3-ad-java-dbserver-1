b2b9fef9ff4cc1d31690f1bcec2b17dfca7c40f3
romulo krebs <romulo.krebs@gmail.com>
2019-12-08 21:53:19 -0300

    endpoint logs com parametros

--- central-erros/src/main/java/br/com/central/erros/impl/business/entity/enums/BuscaPor.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/entity/enums/BuscaPor.java
@@ -2,44 +2,30 @@ package br.com.central.erros.impl.business.entity.enums;
 
 import br.com.central.erros.impl.business.entity.V1.LogV1;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-public enum BuscaPor {
+public class BuscaPor {
 
-    DESCRICAO {
-        @Override
-        public List<LogV1> metodoBuscarPor(List<LogV1> listaInput, String detalhes) {
-            List<LogV1> lista = listaInput.stream().filter(logV1 -> logV1.getDetalhes().toLowerCase().contains(detalhes)).collect(Collectors.toList());
-            return lista;
-        }
-    },
+    ;
 
-        LEVEL {
-            @Override
-            public List<LogV1> metodoBuscarPor(List<LogV1> listaInput, String detalhes) {
+    private String metodo;
 
-                List<LogV1> lista = listaInput.stream().filter(logV1 -> logV1.getLevel().equals(Level.valueOf(detalhes))).collect(Collectors.toList());
-                return lista;
-            }
-
-        },
-
-    ORIGEM {
-
-        @Override
-        public List<LogV1> metodoBuscarPor(List<LogV1> listaInput, String detalhes) {
-            List<LogV1> lista = listaInput.stream().filter(logV1 -> logV1.getIp().equals(detalhes)).collect(Collectors.toList());
-            return lista;
-        }
-    };
-
-    public abstract List<LogV1> metodoBuscarPor(List<LogV1> listaInput, String detalhes);
-
+    private BuscaPor(String metodo){
+        this.metodo = metodo;
     }
 
 
+    public List<LogV1> getMetodo(List<LogV1> listaInput, Integer id) {
+        List<LogV1> lista = listaInput.stream().filter(logV1 -> logV1.getUserV1().getId().equals(id)).collect(Collectors.toList());
+        return lista;
+    }
+
+
+    //  List<LogV1> lista = listaInput.stream().filter(logV1 -> logV1.getLevel().getDescricao().equals(level)).collect(Collectors.toList());
 
 
 
+    //  List<LogV1> lista = listaInput.stream().filter(logV1 -> logV1.getDetalhes().equals(descricao)).collect(Collectors.toList());
+
+}
