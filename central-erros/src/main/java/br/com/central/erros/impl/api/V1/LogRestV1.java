c7e67a86c039e60cab8403272f306f9827e6eaa5
romulo krebs <romulo.krebs@gmail.com>
2019-12-08 21:53:19 -0300

    endpoint logs com parametros

--- central-erros/src/main/java/br/com/central/erros/impl/api/V1/LogRestV1.java
+++ central-erros/src/main/java/br/com/central/erros/impl/api/V1/LogRestV1.java
@@ -7,20 +7,19 @@ import java.util.Optional;
 
 import br.com.central.erros.impl.api.V1.contracts.LogRestEndpointV1;
 import br.com.central.erros.impl.business.dto.LogDTOV1;
-import br.com.central.erros.impl.business.entity.enums.Ambiente;
 import br.com.central.erros.impl.business.entity.enums.BuscaPor;
-import br.com.central.erros.impl.business.entity.enums.OrdenarPor;
 import br.com.central.erros.impl.business.service.V1.LogServiceImplV1;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import br.com.central.erros.impl.business.exception.exceptions.ObjectNotFoundException;
 
 @RestController
 @RequestMapping({"/v1/logs"})
+@Api(value = "Logs",  tags = { "Logs" })
 public class LogRestV1 implements LogRestEndpointV1 {
 
 
@@ -31,19 +30,14 @@ public class LogRestV1 implements LogRestEndpointV1 {
         this.logServiceImplV1 = logServiceImplV1;
     }
 
-
-
-    @GetMapping(path = "/buscaTodos", produces = "application/vnd.central.erros.user-v1+json")
+    @GetMapping(path = "/findAll", produces = "application/vnd.central.erros.user-v1+json")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", required = true, dataType = "string", paramType = "header", value = "Token de autenticação.")
     })
-    @ApiOperation(value = "Retorna todos os logs com o parâmetros selecionados. ", response = LogDTOV1.class)
-    public ResponseEntity<List<LogDTOV1>> buscaLogsListComParametros(@RequestParam(required = true, defaultValue = "PRODUCTION") Ambiente ambiente,
-                                                                     @RequestParam(required = false) Optional<OrdenarPor> ordenarPor,
-                                                                     @RequestParam(required = false) Optional<BuscaPor> buscarPor,
-                                                                     @RequestParam(required = false)  Optional<String> descricaoBusca) {
+    @ApiOperation(value = "Retorna todos os logs. ", response = LogDTOV1.class)
+    public ResponseEntity<List<LogDTOV1>> buscaLogsList() {
 
-        ResponseEntity<List<LogDTOV1>> logOK = ResponseEntity.ok(logServiceImplV1.buscarTodosLogsDoUsuario(ambiente, ordenarPor, buscarPor, descricaoBusca));
+        ResponseEntity<List<LogDTOV1>> logOK = ResponseEntity.ok(logServiceImplV1.buscarTodosLogs());
 
         if (Objects.isNull(logOK.getBody())) {
             logOK = ResponseEntity.noContent().build();
@@ -51,6 +45,22 @@ public class LogRestV1 implements LogRestEndpointV1 {
         return logOK;
     }
 
+    @GetMapping(path = "/", produces = "application/vnd.central.erros.user-v1+json")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", required = true, dataType = "string", paramType = "header", value = "Token de autenticação.")
+    })
+    @ApiOperation(value = "Retorna todos os logs com o parâmetros selecionados. ", response = LogDTOV1.class)
+    public ResponseEntity<List<LogDTOV1>> buscaLogsListComParametros(@RequestParam(required = true, defaultValue = "prod") String ambiente,
+                                                                     @RequestParam(required = false) Optional<String> ordenarPor,
+                                                                     @RequestParam(required = false) Optional<BuscaPor> buscarPor) {
+
+        ResponseEntity<List<LogDTOV1>> logOK = ResponseEntity.ok(logServiceImplV1.buscarTodosLogsDoUsuario(ambiente, ordenarPor, buscarPor));
+
+        if (Objects.isNull(logOK.getBody())) {
+            logOK = ResponseEntity.noContent().build();
+        }
+        return logOK;
+    }
 
     @Override
     @PostMapping("/")
@@ -63,8 +73,6 @@ public class LogRestV1 implements LogRestEndpointV1 {
         logServiceImplV1.salvarNovoLog(logRequest);
         return ResponseEntity.ok().build();
     }
-
-
 
     @Override
     @GetMapping(path = "{id}")
