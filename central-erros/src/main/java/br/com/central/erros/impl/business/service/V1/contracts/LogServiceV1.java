e2d3605ffe7d4e6dc5865865e4312cfd32f43366
romulo krebs <romulo.krebs@gmail.com>
2019-12-09 22:28:32 -0300

    teste com o endpoint de logs com parametros

--- central-erros/src/main/java/br/com/central/erros/impl/business/service/V1/contracts/LogServiceV1.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/service/V1/contracts/LogServiceV1.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import br.com.central.erros.impl.business.dto.LogDTOV1;
 import br.com.central.erros.impl.business.entity.V1.LogV1;
+import br.com.central.erros.impl.business.entity.enums.Ambiente;
 import br.com.central.erros.impl.business.entity.enums.BuscaPor;
 
 public interface LogServiceV1 {
@@ -14,7 +15,8 @@ public interface LogServiceV1 {
 
     List<LogDTOV1> buscarTodosLogs();
 
-    List<LogDTOV1> buscarTodosLogsDoUsuario(String ambiente,  Optional<String> ordenarPor, Optional<BuscaPor> buscarPor);
+
+    List<LogDTOV1> buscarTodosLogsDoUsuario(Ambiente ambiente, Optional<String> ordenarPor, String buscarPor, String descricaoBusca);
 
     LogDTOV1 salvarNovoLog(LogDTOV1 logDTOV1);
 }
