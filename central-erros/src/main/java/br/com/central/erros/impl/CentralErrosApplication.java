12645c6c25499adce7a7f934e18e79d1afb90f7b
romulo krebs <romulo.krebs@gmail.com>
2019-12-10 16:11:06 -0300

    ajuste para optional endpoint de logs e criacao OrdenarPor

--- central-erros/src/main/java/br/com/central/erros/impl/CentralErrosApplication.java
+++ central-erros/src/main/java/br/com/central/erros/impl/CentralErrosApplication.java
@@ -64,9 +64,9 @@ public class CentralErrosApplication implements CommandLineRunner {
         LogV1 logV8 = new LogV1(null, "12313212",16l, LocalDate.now(), "teste", "teste2 ", Ambiente.HOMOLOGATION, Level.ERROR,
                 user4);
         LogV1 logV9 = new LogV1(null, "12313212",16l, null, "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.ERROR, user1);
-        LogV1 logV10 = new LogV1(null, "12313212",16l, null, "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.DEBUG, user2);
-        LogV1 logV11 = new LogV1(null, "12313212",16l, null, "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.WARNING, user3);
-        LogV1 logV12 = new LogV1(null, "111111111",16l, LocalDate.now(), "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.ERROR,
+        LogV1 logV10 = new LogV1(null, "12313212",26l, null, "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.DEBUG, user2);
+        LogV1 logV11 = new LogV1(null, "12313212",36l, null, "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.WARNING, user3);
+        LogV1 logV12 = new LogV1(null, "111111111",46l, LocalDate.now(), "teste", "teste3 ", Ambiente.DEVELOPMENT, Level.ERROR,
                 user4);
 
         logRepositoryV1.saveAll(Arrays.asList(logV1, logV2, logV3 ,logV4,logV5 ,logV6,logV7 ,logV8,logV9 ,logV10,logV11 ,logV12 ));
