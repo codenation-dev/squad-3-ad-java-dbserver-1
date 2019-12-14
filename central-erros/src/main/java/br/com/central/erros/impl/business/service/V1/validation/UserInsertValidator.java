6a28b45ae33739920e3be5b82f20a1e488bfcb41
Samuel <samuelsimao.dev@gmail.com>
2019-12-14 10:04:05 -0300

    Enum

--- central-erros/src/main/java/br/com/central/erros/impl/business/service/V1/validation/UserInsertValidator.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/service/V1/validation/UserInsertValidator.java
@@ -24,11 +24,11 @@ public class UserInsertValidator implements ConstraintValidator<UserInsert, User
     public boolean isValid(UserDTOV1 objDto, ConstraintValidatorContext context) {
         List<FieldMessage> list = new ArrayList<>();
 
-        if (objDto.getTipo().getCod() - 1 == 1 && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+        if (objDto.getTipo().getCod() == 0 && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
         }
 
-        if (objDto.getTipo().getCod() - 1 == 2 && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+        if (objDto.getTipo().getCod() == 1 && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
         }
 
