eeb76c4589373f2a6a811e041f8a20a9ff532900
romulo krebs <romulo.krebs@gmail.com>
2019-12-13 23:26:22 -0300

    commit para atualizar branch

--- central-erros/src/main/java/br/com/central/erros/impl/api/V1/contracts/LogRestEndpointV1.java
+++ central-erros/src/main/java/br/com/central/erros/impl/api/V1/contracts/LogRestEndpointV1.java
@@ -6,11 +6,18 @@ import java.util.Optional;
 
 import br.com.central.erros.impl.business.dto.LogDTOV1;
 import br.com.central.erros.impl.business.dto.UserDTOV1;
+import br.com.central.erros.impl.business.entity.enums.Ambiente;
+import br.com.central.erros.impl.business.entity.enums.BuscaPor;
+import br.com.central.erros.impl.business.entity.enums.OrdenarPor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface LogRestEndpointV1 {
 
-    ResponseEntity<List<LogDTOV1>> buscaLogsList();
+    ResponseEntity<List<LogDTOV1>> buscaLogsListComParametros(Ambiente ambiente,
+                                                             Optional<OrdenarPor> ordenarPor,
+                                                             Optional<BuscaPor> buscarPor,
+                                                             Optional<String> descricaoBusca);
 
     ResponseEntity<Void> adicionaLog(LogDTOV1 logDTOV1);
 
