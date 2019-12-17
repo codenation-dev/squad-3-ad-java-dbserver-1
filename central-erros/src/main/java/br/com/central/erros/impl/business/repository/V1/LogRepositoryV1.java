ddb59da453ddb0d8a09114e13bd92cb41071ba4a
romulo krebs <romulo.krebs@gmail.com>
2019-12-17 17:17:17 -0300

    restaura metodo findAll endpoint logs

--- central-erros/src/main/java/br/com/central/erros/impl/business/repository/V1/LogRepositoryV1.java
+++ central-erros/src/main/java/br/com/central/erros/impl/business/repository/V1/LogRepositoryV1.java
@@ -2,6 +2,7 @@ package br.com.central.erros.impl.business.repository.V1;
 
 
 import br.com.central.erros.impl.business.entity.V1.LogV1;
+import br.com.central.erros.impl.business.entity.enums.Environment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface LogRepositoryV1 extends JpaRepository<LogV1, Integer> {
-    List<LogV1> findAllByUser_IdAndActiveTrue(Integer id);
+    List<LogV1> findAllByUser_IdAndEnvironmentAndActiveTrue(Integer id, Environment environment);
 }
