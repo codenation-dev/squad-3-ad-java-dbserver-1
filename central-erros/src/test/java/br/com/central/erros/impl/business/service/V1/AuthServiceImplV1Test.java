96ea4fa15582ae610fc33a33c32ecfa1bbd23184
Samuel <samuelsimao.dev@gmail.com>
2019-12-17 12:58:40 -0300

    termina teste de auth

--- central-erros/src/test/java/br/com/central/erros/impl/business/service/V1/AuthServiceImplV1Test.java
+++ central-erros/src/test/java/br/com/central/erros/impl/business/service/V1/AuthServiceImplV1Test.java
@@ -1,15 +1,20 @@
 package br.com.central.erros.impl.business.service.V1;
 
+import br.com.central.erros.impl.business.dto.UserDTOV1;
 import br.com.central.erros.impl.business.dto.VerificationCodeDTO;
+import br.com.central.erros.impl.business.entity.enums.UserType;
 import br.com.central.erros.impl.business.exception.exceptions.ValidationException;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AuthServiceImplV1Test {
@@ -25,19 +30,23 @@ public class AuthServiceImplV1Test {
     @Test(expected = ValidationException.class)
     public void returnsExceptionIfCodeIsInvalid() {
         final VerificationCodeDTO dto = new VerificationCodeDTO("1234", "hello@mail.com");
-        Mockito.when(verificationCodeService.isValid(any())).thenReturn(false);
+        when(verificationCodeService.isValid(any())).thenReturn(Boolean.FALSE);
         authService.updateUserPassword(dto, "newPassword");
     }
 
-    /*@Test
+    @Test
     public void updatesUserPasswordAndDeletesToken() {
-        final VerificationCodeDTO code = new VerificationCodeDTO("1234", "a@mail.com");
-        final UserDTOV1 user = new UserDTOV1(1, "User", "a@mail.com", "1234", UserType.PESSOAFISICA, "old");
-        Mockito.when(userService.findByEmail(code.getEmail())).thenReturn(user);
-        Mockito.when(verificationCodeService.isValid(any())).thenReturn(true);
-        Mockito.when(userService.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
-        Mockito.verify(userService, times(1)).save(argThat(argument -> argument.getPassword().equals("newPasword")));
-        Mockito.verify(verificationCodeService, times(1)).delete(code.getEmail());
-        authService.updateUserPassword(code, "newPassword");
-    }*/
+        UserDTOV1 userDTO = new UserDTOV1("John", "a@mail.com", "123", UserType.PESSOAFISICA, "oldPassword");
+        VerificationCodeDTO verificationCodeDTO = new VerificationCodeDTO("abcd", userDTO.getEmail());
+        when(verificationCodeService.isValid(any())).thenReturn(Boolean.TRUE);
+        when(userService.findByEmail("a@mail.com")).thenReturn(userDTO);
+        when(userService.save(any(UserDTOV1.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        ArgumentCaptor<UserDTOV1> captor = ArgumentCaptor.forClass(UserDTOV1.class);
+        authService.updateUserPassword(verificationCodeDTO, "newPassword");
+        verify(userService, times(1)).save(captor.capture());
+        UserDTOV1 actual = captor.getValue();
+        verify(verificationCodeService, times(1)).delete(anyString());
+        Assert.assertEquals("newPassword", actual.getPassword());
+    }
+
 }
