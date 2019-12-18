ce7a585a051715f99897330e7cb5417f1cbbf53c
RodrigoRP <rodrigo.ramos.pa@gmail.com>
2019-12-18 17:26:22 -0300

    Tratamento de excecao.

--- central-erros/src/main/java/br/com/central/erros/impl/api/V1/exception/ResourceExceptionHandler.java
+++ central-erros/src/main/java/br/com/central/erros/impl/api/V1/exception/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -29,6 +30,19 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle(Exception ex,
+                                         HttpServletRequest request, HttpServletResponse response) {
+        if (ex instanceof NullPointerException) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+
+
+
 
 
 }
