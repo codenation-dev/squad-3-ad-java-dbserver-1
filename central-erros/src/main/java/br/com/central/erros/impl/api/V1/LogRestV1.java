214912f0bfbfc953e4c0cb0e371722bbb23b0b5c
samueldcs <mucapapipa.br@gmail.com>
2019-12-19 18:43:44 -0300

    import

--- central-erros/src/main/java/br/com/central/erros/impl/api/V1/LogRestV1.java
+++ central-erros/src/main/java/br/com/central/erros/impl/api/V1/LogRestV1.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.Objects;
